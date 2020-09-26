1228aa126ff4a6e0ae87d22a4d486a608178c4a8
Alex Verrico <contact@alexverrico.com>
2020-09-26 20:48:24 +1000

    Turn on error message for new recoveries by state since that seems to be broken currently

--- covid_parser.py
+++ covid_parser.py
@@ -6,8 +6,8 @@ import json
 
 
 aus_locations = {'aus': 1, 'nsw': 1, 'vic': 2, 'qld': 3, 'sa': 4, 'wa': 5, 'tas': 6, 'nt': 7, 'act': 8}
+aus_states = {'nsw': 1, 'vic': 2, 'qld': 3, 'sa': 4, 'wa': 5, 'tas': 6, 'nt': 7, 'act': 8}
 supported_data_types = ['cases', 'deaths', 'recoveries']
-aus_states = {'nsw': 2, 'vic': 0, 'qld': 4, 'sa': 6, 'wa': 7, 'tas': 8, 'nt': 9, 'act': 10}
 
 
 def rreplace(s, old, _new, occurrence):
@@ -37,18 +37,68 @@ def get_state_new(data_type='cases'):
         statedata = json.loads(data)
         return statedata
     elif data_type == 'recoveries':
-        data = download_data(r'https://e.infogram.com/_/1x9ogDI1RFHnyzW4sfFx?live')
-        junk, data = data.split(r'c19","chart_type_nr":1,"data":', 1)
-        data, junk = data.split(r',"custom":{"showPoints":true,"', 1)
-        data = data.replace(r'\u002F', '/')
-        data = json.loads(data)
-        statedata = list()
-        for i in range(1, len(data[aus_states['nsw']])):
-            tmp = [data[aus_states['nsw']][i][0], data[aus_states['nsw']][i][2], data[aus_states['vic']][i][2],
-                   data[aus_states['qld']][i][2], data[aus_states['sa']][i][2], data[aus_states['wa']][i][2],
-                   data[aus_states['tas']][i][2], data[aus_states['nt']][i][2], data[aus_states['act']][i][2]]
-            statedata.append(tmp)
-        return statedata
+        # data = download_data(r'https://e.infogram.com/_/1x9ogDI1RFHnyzW4sfFx')
+        # junk, data = data.split(r'c19","chart_type_nr":1,"data":', 1)
+        # data, junk = data.split(r',"custom":{"showPoints":true,"', 1)
+        # data = data.replace(r'\u002F', '/')
+        # data = json.loads(data)
+        # statedata = list()
+        # print(data)
+        # for i in range(0, len(data[aus_states['vic']])):
+        #     tmp = [data[aus_states['nsw']][i][0], data[aus_states['nsw']][i][2], data[aus_states['vic']][i][2],
+        #            data[aus_states['qld']][i][2], data[aus_states['sa']][i][2], data[aus_states['wa']][i][2],
+        #            data[aus_states['tas']][i][2], data[aus_states['nt']][i][2], data[aus_states['act']][i][2]]
+        #     statedata.append(tmp)
+        # print(statedata)
+
+        # data = download_data(r'https://e.infogram.com/_/ZPUD4Kmuso7AJ0jLaRlt')
+        # junk, data = data.split(r'164","chart_type_nr":6,"data":', 1)
+        # data, junk = data.split(r',"custom":{"showPoints":true,"', 1)
+        # data = data.replace(r'\u002F', '/')
+        # data = json.loads(data)
+        # # print(data)
+        # statedata = list()
+        # for i in range(2, len(data[aus_states['vic']])):
+        # # for i in range(2, 5):
+        #     temp = list()
+        #     tmp = list()
+        #     iminus = i - 1
+        #     # tmp = [data[aus_locations['nsw']][i][0], str(int(data[aus_locations['nsw']][iminus][1]) - int(data[aus_locations['nsw']][i][1])), data[aus_locations['vic']][i][1],
+        #     #        data[aus_locations['qld']][i][1], data[aus_locations['sa']][i][1], data[aus_locations['wa']][i][1],
+        #     #        data[aus_locations['tas']][i][1], data[aus_locations['nt']][i][1], data[aus_locations['act']][i][1]]
+        #     # for x in aus_locations:
+        #     #     tmp1 = data[aus_locations[x]][i]
+        #     #     y = i - 1
+        #     #     tmp2 = data[aus_locations[x]][y]
+        #     #     print(tmp1)
+        #     #     print(tmp2)
+        #     #     print(str(int(tmp1[1]) - int(tmp2[1])))
+        #     temp = data[aus_states['nsw']][i][0]
+        #     tmp.append(temp)
+        #     for loc in aus_states:
+        #         temp = str(int(data[aus_locations[loc]][iminus][1].replace(',', '')) - int(data[aus_locations[loc]][i][1].replace(',', '')))
+        #         tmp.append(temp)
+        #     print(tmp)
+        #     statedata.append(tmp)
+
+        vicdata = download_data(r'https://www.dhhs.vic.gov.au/victorian-coronavirus-covid-19-data')
+        junk, vicdata = vicdata.split("""<div class="lvn-body">
+    		<div class="row">
+    			<div class="col-xs-6 col-sm-3">
+    				<div class="lvn-box lvn-box-top">""")
+        vicdata, junk = vicdata.split("""</div>
+    				<div class="lvn-box lvn-box-bottom">
+    					<h4>782</h4>
+    					<p>total lives lost</p>
+    				</div>
+    			</div>
+    			<div class="col-xs-6 col-sm-3">""")
+        vicdata = vicdata.replace('\t', '')
+        vicdata = vicdata.split('\n')
+        print(vicdata)
+        # vicdata = vicdata[]
+
+        # return statedata
 
 
 def get_aus_new(data_type='cases'):
@@ -67,23 +117,25 @@ def get_aus_new(data_type='cases'):
         ausdata = json.loads(data)
         return ausdata
     elif data_type == 'recoveries':
-        data = download_data(r'https://e.infogram.com/_/1x9ogDI1RFHnyzW4sfFx?live')
-        junk, data = data.split(r'c19","chart_type_nr":1,"data":', 1)
-        data, junk = data.split(r',"custom":{"showPoints":true,"', 1)
-        data = data.replace(r'\u002F', '/')
-        data = data.replace('""', '"0"')
-        data = json.loads(data)
-        ausdata = list()
-        for i in range(1, len(data[aus_states['nsw']])):
-            tmp = [data[aus_states['nsw']][i][0], str(int(data[aus_states['nsw']][i][2].replace(',', '')) +
-                   int(data[aus_states['vic']][i][2].replace(',', '')) +
-                   int(data[aus_states['qld']][i][2].replace(',', '')) +
-                   int(data[aus_states['sa']][i][2].replace(',', '')) +
-                   int(data[aus_states['wa']][i][2].replace(',', '')) +
-                   int(data[aus_states['tas']][i][2].replace(',', '')) +
-                   int(data[aus_states['nt']][i][2].replace(',', '')) +
-                   int(data[aus_states['act']][i][2].replace(',', '')))]
-            ausdata.append(tmp)
+        # data = download_data(r'https://e.infogram.com/_/1x9ogDI1RFHnyzW4sfFx?live')
+        # junk, data = data.split(r'c19","chart_type_nr":1,"data":', 1)
+        # data, junk = data.split(r',"custom":{"showPoints":true,"', 1)
+        # data = data.replace(r'\u002F', '/')
+        # data = data.replace('""', '"0"')
+        # data = json.loads(data)
+        # ausdata = list()
+        # for i in range(1, len(data[aus_states['nsw']])):
+        #     tmp = [data[aus_states['nsw']][i][0], str(int(data[aus_states['nsw']][i][2].replace(',', '')) +
+        #            int(data[aus_states['vic']][i][2].replace(',', '')) +
+        #            int(data[aus_states['qld']][i][2].replace(',', '')) +
+        #            int(data[aus_states['sa']][i][2].replace(',', '')) +
+        #            int(data[aus_states['wa']][i][2].replace(',', '')) +
+        #            int(data[aus_states['tas']][i][2].replace(',', '')) +
+        #            int(data[aus_states['nt']][i][2].replace(',', '')) +
+        #            int(data[aus_states['act']][i][2].replace(',', '')))]
+        #     ausdata.append(tmp)
+        ausdata = get_country_new('australia', data_type='recoveries')
+        # print(ausdata)
         return ausdata
 
 
@@ -155,10 +207,20 @@ def new(location='aus', data_type='cases'):
         return "unsupportedDataType"
     else:
         if location in aus_locations:
+
             if location == 'aus':
-                data = get_aus_new(data_type=data_type)
+                if data_type == 'recoveries':
+                    data = get_country_new('australia', data_type='recoveries')
+                    parsed_data = [data[-1], data[-2]]
+                    return parsed_data
+                else:
+                    data = get_aus_new(data_type=data_type)
             else:
-                data = get_state_new(data_type=data_type)
+                if data_type == 'recoveries':
+                    return "unsupportedDataType"
+                else:
+                    data = get_state_new(data_type=data_type)
+            # print(data)
             parsed_data = [data[-1][aus_locations[location]], data[-2][aus_locations[location]]]
         else:
             try:
@@ -172,4 +234,4 @@ def new(location='aus', data_type='cases'):
 # def total(location='aus', data_type='cases'):
 #     print(location)
 
-# print(new(data_type='recoveries', location='china'))
+# print(new(data_type='recoveries', location='usa'))
